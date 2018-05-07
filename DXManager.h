3a051dbc9828ac73cb48d821206df7e3e62fa3a6
tonarinohito <rsp44316@gmail.com>
2018-05-07 17:15:48 +0900

    ヘッダ追加

--- DXManager.h
+++ DXManager.h
@@ -4,7 +4,9 @@
 #include <dxgi.h>
 #include <d3dcommon.h>
 #include <d3d11.h>
+#include <D2D1.h>
 
+#pragma comment(lib,"d2d1.lib")
 #pragma comment(lib,"d3d11.lib")
 #pragma comment(lib,"dxgi.lib")
 #pragma comment(lib,"d3dCompiler.lib")
